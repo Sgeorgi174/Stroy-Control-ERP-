50e0d133a01e359a773677436ebacd3c84a648f0
Georgi <sgeorgi174@gmail.com>
2025-05-30 20:21:01 +0500

    added tool controllers

--- nest-js-server/src/tool/tool.controller.ts
+++ nest-js-server/src/tool/tool.controller.ts
@@ -1,7 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { ToolService } from './tool.service';
+import { CreateDto } from './dto/create.dto';
+import { UpdateDto } from './dto/update.dto';
+import { Authorization } from 'src/auth/decorators/auth.decorator';
+import { Roles } from 'generated/prisma';
 
-@Controller('tool')
+@Controller('tools')
 export class ToolController {
   constructor(private readonly toolService: ToolService) {}
+
+  @Authorization(Roles.OWNER)
+  @Post('create')
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() dto: CreateDto) {
+    return this.toolService.create(dto);
+  }
+
+  @Authorization(Roles.OWNER)
+  @Get('all')
+  async getAll() {
+    return this.toolService.getAll();
+  }
+
+  @Authorization(Roles.OWNER)
+  @Get('by-id/:id')
+  async getById(@Param('id') id: string) {
+    return this.toolService.getById(id);
+  }
+
+  @Authorization(Roles.OWNER)
+  @Put('update/:id')
+  async update(@Param('id') id: string, @Body() dto: UpdateDto) {
+    return this.toolService.update(id, dto);
+  }
+
+  @Authorization(Roles.OWNER)
+  @Delete('delete/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: string) {
+    return this.toolService.delete(id);
+  }
 }
