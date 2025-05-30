afea7f3f3a5ffb12d57f4def2430cd6fb5aaa1d8
Georgi <sgeorgi174@gmail.com>
2025-05-30 21:07:59 +0500

    added dto, controllers and services for clothes

--- nest-js-server/src/clothes/clothes.controller.ts
+++ nest-js-server/src/clothes/clothes.controller.ts
@@ -1,7 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ClothesService } from './clothes.service';
+import { Authorization } from 'src/auth/decorators/auth.decorator';
+import { Roles } from 'generated/prisma';
+import { CreateDto } from './dto/create.dto';
+import { UpdateDto } from './dto/update.dto';
 
 @Controller('clothes')
 export class ClothesController {
   constructor(private readonly clothesService: ClothesService) {}
+
+  @Authorization(Roles.OWNER)
+  @Post('create')
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() dto: CreateDto) {
+    return this.clothesService.create(dto);
+  }
+
+  @Authorization(Roles.OWNER)
+  @Get('all')
+  async getAll() {
+    return this.clothesService.getAll();
+  }
+
+  @Authorization(Roles.OWNER)
+  @Get('by-id/:id')
+  async getById(@Param('id') id: string) {
+    return this.clothesService.getById(id);
+  }
+
+  @Authorization(Roles.OWNER)
+  @Put('update/:id')
+  async update(@Param('id') id: string, @Body() dto: UpdateDto) {
+    return this.clothesService.update(id, dto);
+  }
+
+  @Authorization(Roles.OWNER)
+  @Delete('delete/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: string) {
+    return this.clothesService.delete(id);
+  }
 }
--- nest-js-server/src/clothes/clothes.service.ts
+++ nest-js-server/src/clothes/clothes.service.ts
@@ -1,4 +1,86 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateDto } from './dto/create.dto';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
+import { UpdateDto } from './dto/update.dto';
 
 @Injectable()
-export class ClothesService {}
+export class ClothesService {
+  public constructor(private readonly prismaService: PrismaService) {}
+
+  public async create(dto: CreateDto) {
+    try {
+      return await this.prismaService.clothes.create({
+        data: {
+          name: dto.name,
+          size: dto.size,
+          price: dto.price,
+          quantity: dto.quantity,
+          inTransit: dto.inTransit,
+          objectId: dto.objectId,
+          type: dto.type,
+          season: dto.season,
+        },
+      });
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      )
+        throw new ConflictException(
+          'Инструмент с таким серийным номером уже существует',
+        );
+
+      throw new InternalServerErrorException(
+        'Ошибка создания нового инструмента',
+      );
+    }
+  }
+
+  public async getById(id: string) {
+    const clothes = await this.prismaService.clothes.findUnique({
+      where: { id },
+    });
+
+    if (!clothes) throw new NotFoundException('Инструмент не найден');
+
+    return clothes;
+  }
+
+  public async getAll() {
+    return await this.prismaService.clothes.findMany();
+  }
+
+  public async update(id: string, dto: UpdateDto) {
+    await this.getById(id);
+
+    const updatedClothes = this.prismaService.clothes.update({
+      where: { id },
+      data: {
+        name: dto.name,
+        size: dto.size,
+        price: dto.price,
+        quantity: dto.quantity,
+        inTransit: dto.inTransit,
+        objectId: dto.objectId,
+        type: dto.type,
+        season: dto.season,
+      },
+    });
+
+    return updatedClothes;
+  }
+
+  public async delete(id: string) {
+    await this.getById(id);
+
+    await this.prismaService.clothes.delete({ where: { id } });
+
+    return true;
+  }
+}
