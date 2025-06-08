82caad7853ef5f3fb92becdbe91659760c12585d
Georgi <sgeorgi174@gmail.com>
2025-06-08 23:15:41 +0500

    delete client side

--- react-ERP-app/src/modules/dashboard/Dashboard.tsx
+++
@@ -1,8 +0,0 @@
-import { useAuthStore } from "../../stores/auth.store";
-
-export function Dashboard() {
-  const { user } = useAuthStore();
-  console.log(user);
-
-  return <div>Dashboard</div>;
-}
