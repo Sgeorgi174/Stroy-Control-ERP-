5bf56ec6a8290896a49180cb9ad6811446fb8bcb
Georgi <sgeorgi174@gmail.com>
2025-11-13 13:53:01 +0500

    changes

--- react-ERP-app/src/pages/employees/Employees.tsx
+++ react-ERP-app/src/pages/employees/Employees.tsx
@@ -29,9 +29,32 @@ export function Employees() {
     },
     activeTab === "employee"
   );
+
+  const positionStats = employees.reduce<Record<string, number>>((acc, emp) => {
+    const pos = emp.position || "Без должности";
+    acc[pos] = (acc[pos] || 0) + 1;
+    return acc;
+  }, {});
+
   return (
     <div>
       <EmployeesFilter />
+      <div className="bg-muted p-2 pl-4 rounded-xl mt-5 flex gap-4">
+        <p>
+          Всего сотрудников:{" "}
+          <span className="font-medium">{employees.length}</span>
+        </p>
+
+        {Object.entries(positionStats).length > 0 && (
+          <div className="space-y-1">
+            {Object.entries(positionStats).map(([position, count]) => (
+              <p key={position}>
+                {position}: <span className="font-medium">{count}</span>
+              </p>
+            ))}
+          </div>
+        )}
+      </div>
       {selectedEmployeeType === "ACTIVE" && (
         <EmployeesTable
           employees={employees}
